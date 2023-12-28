e2a9298521a853e5838e18c20b60461c1d238324
Jim McNeill <jimillustrator@gmail.com>
2023-12-28 12:46:37 -0700

    Simplified Atom Maker
    removed everything but the abstract class

--- Programming Theory Project/Assets/Scripts/AtomMaker.cs
+++ Programming Theory Project/Assets/Scripts/AtomMaker.cs
@@ -9,36 +9,23 @@ public class AtomMaker : MonoBehaviour
 
     private BoxCollider protonCollider;
 
-    public int atomicNumber;
-
-    private float offsetX;
-    public float speed;
-
-
     private void Start()
     {
         protonCollider = proton.GetComponent<BoxCollider>();
         float offsetX = protonCollider.size.x;
-        ProtonCreator();
     }
 
     public virtual void ProtonCreator()
     {
-        for (int i = 0; i < atomicNumber; i++)
+
         {
-            Instantiate(proton, new Vector3((transform.position.x + i)/2, transform.position.y, (transform.position.z + i)/2), transform.rotation);
-            Debug.Log(i);
+            Instantiate(proton, transform.position, transform.rotation);
         }
     }
 
     public virtual void ElectronCreator()
     {
         Instantiate(electron, transform.position, transform.rotation);
-    }
-
-    public void AtomDrop()
-    {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
 
 }
